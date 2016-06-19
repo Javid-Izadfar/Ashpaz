798044c778f0a18a7cf51c5ba56d60f73fc2e3db
Javid Izadfar <javid.izadfar@gmail.com>
2016-06-19 13:06:35 +0430

    Create index.html
    صفحه ی اصلی ساخته شد.
    توجه: هنوز فرم ثبت نام و ورود تکمیل نشده است.
    توجه: نواره کناری به توجه بیشتری نیاز دارد.

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,23 @@
 // Add custom js here
+(function( $ ) {
+
+  $('.navside.dynamic-navside').navside({
+    toggleSpeed : 220
+  }); // activate navside
+
+  $('.joqd.input').each(function(){
+    $(this).input({
+      openLabelColor: 'red',
+      openInputColor: 'red'
+    });
+  })
+
+  $('#isChefSwitchLabel').click(function(){
+    if($('#isChef').prop('checked')){
+      $('#cater').parent().fadeIn();
+    } else {
+      $('#cater').parent().fadeOut();
+    }
+  })
+
+}( jQuery ));
